dfca68d690e3ede916ab4979039d92aecb90c95b
htyxz8802 <yjqt9758>
2016-04-29 16:51:00 +0800

    test Material Design CollapsingToolbarLayout

--- app/src/main/java/com/example/chat/viewtest/UI/CoordintorActivity.java
+++ app/src/main/java/com/example/chat/viewtest/UI/CoordintorActivity.java
@@ -13,13 +13,18 @@ import com.example.chat.viewtest.R;
 public class CoordintorActivity extends Activity {
 
     private Toolbar toolbar ;
+    private FloatingActionButton bt_FlowLayout ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_coordintor);
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
-        toolbar.setTitleTextColor(ContextCompat.getColor(this,R.color.black));
-        toolbar.setTitle("toolbar");
+//        setContentView(R.layout.activity_coordintor);
+        setContentView(R.layout.coordinator);
+        bt_FlowLayout = (FloatingActionButton) findViewById(R.id.bt_FlowLayout);
+
+
+        toolbar = (Toolbar) findViewById(R.id.test_toolbar);
+//        toolbar.setTitleTextColor(ContextCompat.getColor(this,R.color.black));
+//        toolbar.setTitle("toolbar");
     }
 }
